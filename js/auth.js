7ca7336e37a5228f2762226ec98d4ceda472e763
brajkovicjuan <juanbrajkovic@gmail.com>
2026-06-10 10:28:07 -0300

    Redirige a admin.html si login es exitoso

--- js/auth.js
+++ js/auth.js
@@ -22,5 +22,6 @@ async function login(e) {
     if (respuesta.ok) {
         const data = await respuesta.json();
         sessionStorage.setItem("admin", JSON.stringify(data));
+        window.location.href = "admin.html";
     }
 }
